5497dad972365ee080e60233537c6abd6c2c4e6d
JazzDeveloper <jazmorguzdev@gmail.com>
2018-04-05 11:32:28 -0500

    correcciones

--- JuegoDeLaVidaConway/src/com/dominio/juego_vida_conway/utilidades/Precondiciones.java
+++ JuegoDeLaVidaConway/src/com/dominio/juego_vida_conway/utilidades/Precondiciones.java
@@ -3,6 +3,7 @@ package com.dominio.juego_vida_conway.utilidades;
 import com.dominio.juego_vida_conway.jugar.Coordenada;
 import com.dominio.juego_vida_conway.jugar.Mundo.Célula;
 
+//TODO: revisar esta clase
 public final class Precondiciones {
 	
 	private Precondiciones(){
@@ -23,16 +24,16 @@ public final class Precondiciones {
 	}
 
 	//asegura que la copia no salga de los limites de las matrices que se estan copiando
-		private static boolean precondicionesVentana(final Célula[][] destino, final Célula[][] fuente, final Célula[][] ref,
-															final Coordenada inicioDestino, final Coordenada inicioFuente) {
-				
-			return ref.length > (destino.length - inicioDestino.columna()) || ref.length > (fuente.length - inicioFuente.columna()) ||
-					ref[0].length > (destino[0].length - inicioDestino.fila()) || ref[0].length > (fuente[0].length - inicioFuente.fila());
-		}
+	private static boolean precondicionesVentana(final Célula[][] destino, final Célula[][] fuente, final Célula[][] ref,
+														final Coordenada inicioDestino, final Coordenada inicioFuente) {
+			
+		return ref.length > (destino.length - inicioDestino.columna()) || ref.length > (fuente.length - inicioFuente.columna()) ||
+				ref[0].length > (destino[0].length - inicioDestino.fila()) || ref[0].length > (fuente[0].length - inicioFuente.fila());
+	}
 
-		//asegura que las coordenadas esten dentro del arreglo
-		private static boolean precondicionesCoordenadas(Célula[][] células, Coordenada coordenada){
-			return coordenada.columna() < 0 || coordenada.fila() < 0 || coordenada.columna() >= células.length || coordenada.fila() >= células[0].length;
+	//asegura que las coordenadas esten dentro del arreglo
+	private static boolean precondicionesCoordenadas(Célula[][] células, Coordenada coordenada){
+		return coordenada.columna() < 0 || coordenada.fila() < 0 || coordenada.columna() >= células.length || coordenada.fila() >= células[0].length;
 	}
 
 }
